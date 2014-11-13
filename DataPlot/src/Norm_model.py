7301c29cb89216575e224239c80bc9df2ed4c644
Manrich van Greunen <manrich121@gmail.com>
2014-11-13 15:37:29 +0200

    Implemented Normal Distribution (Gaussian) model and evaluated on 100 machines

---
+++ DataPlot/src/Norm_model.py
@@ -0,0 +1,37 @@
+'''
+Created on 13 Nov 2014
+
+@author: Manrich
+'''
+import numpy as np
+
+class Norm_model(object):
+    '''
+    Normal (Gaussian) distribution Model. 
+    '''
+
+    def __init__(self, data):
+        '''
+        Create a normal distribution model
+        '''
+        self.data = data[:]
+        
+    def fit(self):
+        '''
+        Fits a Gaussian model on the training data by calculating parameters mu and sigma
+        '''
+        self.mu = np.mean(self.data)
+        self.sigma = np.std(self.data)
+    
+    def update(self, data):
+        '''
+        Updates the model training data and refits the Gaussian model
+        '''
+        self.data = data
+        self.fit()
+    
+    def predict(self, fc):
+        '''
+        Samples the Normal distribution to obtain fc number of samples
+        '''
+        return np.random.normal(self.mu, self.sigma, fc)
