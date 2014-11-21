077f63eba57d4ba2383cdb8898437d6f080ea1ca
Manrich van Greunen <manrich121@gmail.com>
2014-11-21 11:33:37 +0200

    Did 2nd order MC and Evaluated both 1st and 2nd order

--- DataPlot/src/Markov_model.py
+++ DataPlot/src/Markov_model.py
@@ -10,43 +10,58 @@ class Markov_model(object):
     '''
     classdocs
     '''
-    def __init__(self, data, M=40):
+    def __init__(self, data, M=40, order=1):
         '''
         Constructor
         '''
         self.data = data[:]        
         self.bins = np.array(np.linspace(0,max(data), M+1))
+        self.states = np.digitize(data, self.bins, right=True)
         self.M = M
+        self.order = order
           
     def fit(self):
         '''
-        Initialises the transition matrix using the fequency of occurance and Laplace method
+        Initialises the transition matrix using the frequency of occurance and Laplace method
         '''
-#         transmat = np.ones((self.M,self.M))
-        transmat = np.zeros((self.M,self.M))
-        for n in range(self.data.shape[0]-1):
-            i, j = self.getState(self.data[n])-1 , self.getState(self.data[n+1])-1
-            transmat[i, j] += 1
-        transmat = transmat / np.tile(np.sum(transmat,axis=1), (self.M,1)).T
-        self.transmat =  transmat
-            
+        if self.order == 1:
+            transmat = np.zeros((self.M,self.M))
+            for n in range(self.data.shape[0]-1):
+                i, j = self.states[n]-1 , self.states[n+1]-1
+                transmat[i, j] += 1
+            transmat = transmat / np.tile(np.sum(transmat,axis=1), (self.M,1)).T
+            self.transmat =  transmat
+        elif self.order == 2:
+            M = self.M
+            data = self.states
+            trans = np.array([np.zeros((M,M))]*M)
+            for i in range(1,len(data)-1):
+                x, y, z = data[i-1], data[i], data[i+1]
+                trans[x-1,y-1,z-1] += 1
+         
+            trans = np.divide(trans, np.array([np.tile(np.sum(trans[idx], axis=1), (M,1)).T for idx in range(M)]))
+            self.transmat = np.nan_to_num(trans)
+
     def update(self,data):
         self.data = data[:]
-        self.bins = np.array(np.linspace(0,max(data), self.M+1))
+        self.bins = np.array(np.linspace(0,max(self.data), self.M+1))
+        self.states = np.digitize(self.data, self.bins, right=True)
         self.fit()
     
     def predict(self, fc):
-        forecasts = np.zeros((fc,1))
-        transback = self.transmat
-        for p in range(1,fc+1):
-            cur_state = np.zeros((1,self.M))
-            cur_state[0, self.getState(self.data[-1])-1] = 1 
-            transrow =  np.dot(cur_state, self.transmat**p).squeeze()
-            pred_state = np.argmax(transrow)+1
-            forecasts[p-1,0] = self.bins[pred_state]
-            self.transmat = np.dot(self.transmat, self.transmat) 
-        self.transmat = transback
+        forecasts = np.zeros((fc))
+        cur_state = self.states[-1] 
+        if self.order == 1: 
+            for p in range(1,fc+1):
+                transrow = self.transmat[cur_state-1,:]
+                pred_state = np.argmax(transrow)+1
+                cur_state = pred_state
+                forecasts[p-1] = self.bins[pred_state]
+        elif self.order == 2:
+            prev_state= self.states[-2]
+            for p in range(fc):
+                pred = np.argmax(self.transmat[prev_state-1, cur_state-1]) +1
+                prev_state = cur_state
+                cur_state = pred
+                forecasts[p] = self.bins[pred]
         return forecasts
-     
-    def getState(self,val):
-        return np.argmin(val>self.bins)
--- DataPlot/src/markov_cpudata.py
+++ DataPlot/src/markov_cpudata.py
@@ -5,7 +5,7 @@ import Markov_model
 
 from multiprocessing import Pool as ThreadPool 
 
-def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_window=3000, predic_window=30):
+def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_window=3000, predic_window=30, order_=2):
     '''
     Input window = 250 hours = 250*12 = 3000 
     look ahead window 60 samples =  5 hours = 720min/5 = 60
@@ -18,17 +18,17 @@ def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_wind
     for strIndex in range(0,N-input_window - predic_window, step):
         if strIndex == 0:
             y = data[:input_window,1]
-            model = Markov_model.Markov_model(y)
+            model = Markov_model.Markov_model(y, order=order_)
             model.fit()
         else:
             y = data[strIndex:strIndex+input_window,1]
             model.update(y)
               
         y_pred = model.predict(predic_window)
-        y_pred[y_pred[:,0]<0,0] = minimum
-        result.append(y_pred[:,0])
+        y_pred[y_pred<0] = minimum
+        result.append(y_pred)
     f = filename.split('/')[-1]
-    fileutils.writeCSV("d:/data/cpu_markov_forecasts/"+f, np.atleast_2d(result))
+    fileutils.writeCSV("d:/data/cpu_markov"+str(order_)+"_forecasts/"+f, np.atleast_2d(result))
     print filename, "complete!"
 
 if __name__ == '__main__':
