9f965354aa708292b88cd0f46b58011ece895a1e
Manrich van Greunen <manrich121@gmail.com>
2014-11-28 10:11:40 +0200

    Evaluated Press method on all machines. Further investigation is needed in order to improve dominant period calculation

---
+++ DataPlot/src/Press_model.py
@@ -0,0 +1,120 @@
+'''
+Created on 27 Nov 2014
+
+@author: Manrich
+'''
+import numpy as np
+from scipy import signal, stats
+import dtw
+import tsutils
+from Markov_model import Markov_model
+
+class Press_model(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, data, maximum, corr_tresh=0.85, mean_ratio=0.1, match_rate=1.0, filter_window=21):
+        '''
+        Create a Signature Pattern recognizer Markov based on PRESS's signature-driven method
+        
+        Params
+        ------
+        @param data: Training data
+        @type data: Array-like
+        @param corr_tresh: Patterns should have a Pearson-Correlation above corr_tresh
+        @type corr_tresh: float
+        @param mean_ratio: 
+        '''
+        self.history = data[:]
+        self.data = data[:]
+        self.corr_thres = corr_tresh
+        self.mr = mean_ratio
+        self.window = filter_window
+        self.match = match_rate
+        self.N = data.shape[0]
+        self.contain_pat = False
+        self.warp = False
+        self.markov = Markov_model(data=data, maximum=maximum)
+        
+    def fit(self):
+        '''
+        Fits a model: First smoothes the data using a Median filter. Then determines if a pattern exists
+        by calculating the dominant period and splitting the time series into pattern windows.
+        For each adjacent pair of the pattern widows, the Pearson-Correlation is calculated and compared 
+        to the threshold together with the two pattern means. If the pattern exists for more than the match_ratio
+        a Signature pattern is calculated using the original data and pattern windows that match.
+        
+        **Note: If no pattern is found, a 1st order Markov model is trained on the data instead
+        '''
+        self.markov.fit()
+        # Smooth data and dominant period
+        smooth = signal.medfilt(self.data, self.window)
+        Z = tsutils.findDominentSeason(self.data)
+#         Z=288
+        # Determine the number of pattern windows and split smoothed data
+        Q = np.int(np.ceil(1.0*self.N/Z))
+        if Q > 2:
+            idx = range(Z,Q*Z,Z)
+            P = np.array(np.split(smooth, idx))
+            self.P = P
+            self.warp = P[-1].shape[0] != P[-2].shape[0]
+            # Check pattern exist
+            PC = []
+            MR = []
+            for i in range(Q-1-self.warp):
+                PC.append(stats.pearsonr(P[i], P[i+1])[0])
+                MR.append(np.mean(P[i])/np.mean(P[i+1]))
+                
+            # Test if pattern exist
+            if np.all(PC>self.corr_thres):
+                self.contain_pat = True
+                Pr = np.array(np.split(self.data, idx))
+                if self.warp:
+                    self.sig = np.average(Pr[:-1],axis=0)
+                else:
+                    self.sig = np.average(Pr,axis=0)
+        
+    def update(self, newData):
+        '''
+        Updates the markov's training data and evaluates the pattern again. If a pattern does not exist
+        call's Markov_models update() function with the new data.
+        '''
+        self.data = np.append(self.data[len(newData):], newData)
+        self.contain_pat = False
+        self.warp = False
+        self.fit()
+        self.markov.update(newData[:])
+        self.history = np.append(self.history, newData)    
+                    
+    def predict(self, fc):
+        '''
+        Uses the Signature pattern and determines the index of the last pattern window, using Dynamic Time Warping
+        and predicts fc samples into the future using the Signature pattern.
+        
+        **Note: Will return the Markov markov predictions if no pattern was found
+        '''
+        forecasts = np.zeros((fc))
+        if self.contain_pat:
+            if self.warp:
+                Z = len(self.sig)
+                index = getWarpIndex(self.P[-1], self.sig)
+                if (index+fc)>=Z:
+                    forecasts = np.array(self.sig[index:])
+                    sub = (index+fc) - Z
+                    forecasts = np.append(forecasts, self.sig[:sub])
+                else:
+                    forecasts = self.sig[index:fc+index]
+            else:
+                forecasts = self.sig[:fc]
+        else:
+            return self.markov.predict(fc)
+        return forecasts
+    
+def getWarpIndex(x, y):
+        dist, _, _= dtw.dtw(x, y, dist=absDist)
+        return np.max(x.shape[0] + np.int(np.ceil(dist)-1),0)
+    
+def absDist(x, y):
+    return abs(x-y)
--- DataPlot/src/press_cpudata.py
+++ DataPlot/src/press_cpudata.py
@@ -1,11 +1,11 @@
 import fileutils
 import numpy as np
 import matplotlib.pyplot as plt
-import Markov_model
+import Press_model
 
 from multiprocessing import Pool as ThreadPool 
 
-def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_window=3000, predic_window=30, order_=1):
+def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_window=3000, predic_window=30):
     '''
     Input window = 250 hours = 250*12 = 3000 
     look ahead window 60 samples =  5 hours = 720min/5 = 60
@@ -19,18 +19,21 @@ def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_wind
     for strIndex in range(0,N-input_window - predic_window, step):
         if strIndex == 0:
             y = data[:input_window,1]
-            model = Markov_model.Markov_model(y, maximum=max, order=order_)
+            model = Press_model.Press_model(y, maximum=max)
             model.fit()
         else:
 #             y = data[strIndex:strIndex+input_window,1]
             y = data[input_window + strIndex - step:input_window + strIndex, 1]
             model.update(y)
-              
         y_pred = model.predict(predic_window)
         y_pred[y_pred<0] = minimum
         result.append(y_pred)
+    res = np.zeros((len(result),predic_window))
+    
+    for i in range(len(result)):
+        res[i,:len(result[i])] = result[i] 
     f = filename.split('/')[-1]
-    fileutils.writeCSV("d:/data/cpu_markov"+str(order_)+"_forecasts/"+f, np.atleast_2d(result))
+    fileutils.writeCSV("d:/data/cpu_press_forecasts/"+f, np.atleast_2d(res))
     print filename, "complete!"
 
 if __name__ == '__main__':
@@ -38,7 +41,7 @@ if __name__ == '__main__':
     pool = ThreadPool(4)
     files =  fileutils.getFilelist("D:/data/cpuRate")
         
-    performsSlidingWindowForecast("D:/data/cpuRate/cpu_155313295.csv")
+    performsSlidingWindowForecast("d:/data/cpuRate/cpu_431038861.csv")
 #     pool.map(performsSlidingWindowForecast, files)
     pool.close()
     pool.join()
