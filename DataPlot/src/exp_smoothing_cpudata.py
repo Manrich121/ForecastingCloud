be96274c33b2c435da738a83e101bcf1a7f5e9e6
Manrich van Greunen <manrich121@gmail.com>
2015-05-14 11:07:09 +0200

    Minor changes to ipython notebooks evaluation plots

--- DataPlot/src/exp_smoothing_cpudata.py
+++ DataPlot/src/exp_smoothing_cpudata.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 from multiprocessing import Pool as ThreadPool 
 
-def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_window=3000, predic_window=60):
+def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_window=3000, predic_window=30):
     '''
     Input window = 250 hours = 250*12 = 3000 
     look ahead window 60 samples =  5 hours = 720min/5 = 60
@@ -32,13 +32,13 @@ def performsSlidingWindowForecast(filename, minpercentile=5, step=30, input_wind
         result.append(y_pred)
     
     f = filename.split('/')[-1]
-    fileutils.writeCSV("D:/Wikipage data/network_hw/"+f, np.atleast_2d(result))
+    fileutils.writeCSV("D:/Wikipage data/pageviews_hw/"+f, np.atleast_2d(result))
     print filename, "complete!"
 
 if __name__ == '__main__':
     aggregatedRmse = None
     pool = ThreadPool(4)
-    files =  fileutils.getFilelist("D:/Wikipage data/network")
+    files =  fileutils.getFilelist("D:/Wikipage data/pageviews")
     
 #     performsSlidingWindowForecast(files[1])
     pool.map(performsSlidingWindowForecast, files)
